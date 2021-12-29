2fdbb0c2082bc6077340b7ff3f2c08fd8f4632bc
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-29 20:47:36 -0300

    Desaclopar variaveis de ambiente com o Python Decouple

--- quiz/settings.py
+++ quiz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-89n&&g+0v8njyyl0q%!a8k+la6l#7@%04co^if4ek$reh#i66^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
