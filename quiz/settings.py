0064e651276f96be20db2a966cb16f00d1140c2b
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-29 20:52:22 -0300

    Desaclopar variaveis de ambiente SECRET_KEY

--- quiz/settings.py
+++ quiz/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-89n&&g+0v8njyyl0q%!a8k+la6l#7@%04co^if4ek$reh#i66^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
