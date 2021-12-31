dfec25afe5850476b01bc9a63cb31f3a642b91d3
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-30 23:30:22 -0300

    Estabelecido parametros da view perguntas.

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -5,4 +5,5 @@ def home(request):
     return render(request, 'base/home.html')
 
 def perguntas(request, indice):
+    contexto = {'indice_da_questao': indice}
     return render(request, 'base/game.html')
