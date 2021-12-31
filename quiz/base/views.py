fc10782b5567904490e45d6204e2997b03cabd3f
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-30 23:35:32 -0300

    Criada view de classificacao

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -6,4 +6,7 @@ def home(request):
 
 def perguntas(request, indice):
     contexto = {'indice_da_questao': indice}
-    return render(request, 'base/game.html')
+    return render(request, 'base/game.html', context=contexto)
+
+def classificacao(request):
+    return render(request, 'base/classificacao.html')
