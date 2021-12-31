4a836f6973c5e99f77fe6bd97b3dcaecf04cb8f4
jasiel Serra <jasiel_serra@hotmail.com>
2021-12-31 00:18:53 -0300

    Migrado do requirements.txt para o Pipenv.

--- quiz/base/admin.py
+++ quiz/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from quiz.base.models import Pergunta
 
 # Register your models here.
+
+@admin.register(Pergunta)
+class PerguntaAdmin(admin.ModelAdmin):
+    pass
--- quiz/base/models.py
+++ quiz/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Pergunta(models.Model):
+    enunciado = models.TextField()
+    alternativas = models.JSONField()
+    disponivel = models.BooleanField(default=False)
+    alternativa_correta = models.IntegerField(choices=[
+        (0, 'A'),
+        (1, 'B'),
+        (2, 'C'),
+        (3, 'D'),
+    ])
