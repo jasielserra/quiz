f44661e6734f505726e9082d091c3e89d6fb72df
Jasiel <jasiel_serra@hotmail.com>
2022-01-02 02:09:10 -0300

    Implementada uma view que recebe um id e retorna uma pergunta com base em uma query.

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
+from quiz.base.models import Pergunta
 # Create your views here.
+
 def home(request):
     return render(request, 'base/home.html')
 
 def perguntas(request, indice):
-    contexto = {'indice_da_questao': indice}
+    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice-1]
+    contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
     return render(request, 'base/game.html', context=contexto)
 
 def classificacao(request):
