336aab0dafbc58f0702890ce42be478bd8185c88
Jasiel <jasiel_serra@hotmail.com>
2022-01-03 13:00:44 -0300

    Implementada lógica de lançar o usuário na sessão caso exista para nossa view de perguntas caso contrário deve ser redirecionado para nossa view de login.

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -26,9 +26,14 @@ def home(request):
     return render(request, 'base/home.html')
 
 def perguntas(request, indice):
-    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice-1]
-    contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
-    return render(request, 'base/game.html', context=contexto)
+    try:
+        aluno_id = request.session['aluno_id']
+    except KeyError:
+        return redirect('/')
+    else:
+        pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice-1]
+        contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
+        return render(request, 'base/game.html', context=contexto)
 
 def classificacao(request):
     return render(request, 'base/classificacao.html')
