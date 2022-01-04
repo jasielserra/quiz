3906ab054eaee051df3cab107e9c7279119dedde
Jasiel <jasiel_serra@hotmail.com>
2022-01-04 03:25:34 -0300

    Lista a classificação dos cincos primeiros alunos

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -63,7 +63,13 @@ def classificacao(request):
     except KeyError:
         return redirect('/')
     else:
-        Resposta.objects.filter(aluno_id=aluno_id).aggregate(Sum('pontos'))
-        pontuacao_do_aluno = 10
-        context = {'pontuacao_do_aluno': pontuacao_do_aluno }
-        return render(request, 'base/classificacao.html')
+        pontos_dct = Resposta.objects.filter(aluno_id=aluno_id).aggregate(Sum('pontos'))
+        pontuacao_do_aluno = pontos_dct['pontos__sum']
+        numero_de_alunos_com_maior_pontuacao = Resposta.objects.values('aluno').annotate(Sum('pontos')).filter(pontos__sum__gt=pontuacao_do_aluno).count()
+        primeiros_alunos_da_classificacao = list(Resposta.objects.values('aluno', 'aluno__nome').annotate(Sum('pontos')).order_by('-pontos__sum')[:5])
+        context = {
+            'pontuacao_do_aluno': pontuacao_do_aluno,
+            'posicao_do_aluno': numero_de_alunos_com_maior_pontuacao + 1,
+            'primeiros_alunos_da_classificacao': primeiros_alunos_da_classificacao,
+                   }
+        return render(request, 'base/classificacao.html', context)
